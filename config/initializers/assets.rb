9091632f6f9e650d4da7378988aeebc5c79dc785
darknawg63 <darknawg63@gmail.com>
2015-01-25 22:27:32 +0100

    Added jquery-ui

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( jquery-1.11.2.min.js )
+Rails.application.config.assets.precompile += %w( jquery-ui-1.11.2/jquery-ui.min.js )
