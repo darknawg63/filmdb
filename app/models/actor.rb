c82f34a3f22f6f5fe65fd204a6a3bd3913cfd429
darknawg63 <darknawg63@gmail.com>
2015-01-25 19:58:35 +0100

    Added full_name Virtual Attribute

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,6 +6,16 @@ class Actor < ActiveRecord::Base
   has_many :roles
   has_many :films, :through => :roles
 
+  def full_name
+    [fname, lname].join(' ')
+  end
+
+  def full_name=(name)
+    split = name.split(' ', 2)
+    self.fname = split.first
+    self.lname = split.last
+  end
+
   def to_s
     "#{fname} #{lname}"
   end
